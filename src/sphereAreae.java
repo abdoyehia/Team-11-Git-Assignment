e0566d3d21795b6807475ef06e1c3a0c9deea371
abdoyehia <abdoyehia85@yahoo.com>
2020-02-28 23:03:56 +0200

    20170153
    simple output change !

--- src/sphereAreae.java
+++ src/sphereAreae.java
@@ -4,7 +4,7 @@ public class sphereAreae implements ISubscriber {
 	@Override
 	public void notifySubscriber(int r) {
 		double area = Math.PI * 4 * r * r;
-		System.out.println("Hello, I am really a simple subscriber and I am notified with "+area);
+		System.out.println("Hellothe area is : "+area);
 		
 	}
 
