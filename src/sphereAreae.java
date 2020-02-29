6d1d2f0d2fdcf460258a7237c9e8d06be949d3c3
abdoyehia <abdoyehia85@yahoo.com>
2020-02-29 19:50:32 +0200

    Revert "Update sphereAreae.java"
    This reverts commit f48ef4ea8bbf24c57cdf3779f08766e311a39b91.

--- src/sphereAreae.java
+++ src/sphereAreae.java
@@ -4,7 +4,7 @@ public class sphereAreae implements ISubscriber {
 	@Override
 	public void notifySubscriber(int r) {
 		double area = Math.PI * 4 * r * r;
-		System.out.println("Hello the area is : "+area);
+		System.out.println("Hellothe area is : "+area);
 		
 	}
 
