c1128fc03f965998fdd5d88369d4229a00e808fe
Sara Shaarawy <36344967+SaraShaarawy@users.noreply.github.com>
2020-03-01 20:20:59 +0200

    Create SphereCircumference.java

---
+++ src/SphereCircumference.java
@@ -0,0 +1,14 @@
+
+public class SphereCircumference {
+	public double circumference(int r)
+	{
+		double sphereCirc = Math.PI * 2 * r;
+		return sphereCirc;
+	}
+	
+	public void notifySubscriber(int input) {
+      System.out.print( "The sphere circumference = : " + circumference(input));
+
+  }
+
+}
